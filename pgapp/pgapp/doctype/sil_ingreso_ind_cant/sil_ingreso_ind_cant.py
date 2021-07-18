9e8dfe0ff72156a6d36677328f9beb0ec76f1713
Orlando Cholota <edwin_orlando83@hotmail.com>
2021-07-18 16:53:34 -0500

    arreglos

---
+++ pgapp/pgapp/doctype/sil_ingreso_ind_cant/sil_ingreso_ind_cant.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, orlando and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class sil_ingreso_ind_cant(Document):
+	pass
