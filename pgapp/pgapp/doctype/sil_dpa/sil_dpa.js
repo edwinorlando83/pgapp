c9ed78abe616a1745f5e657be2e37be49bb5bed2
Orlando Cholota <edwin_orlando83@hotmail.com>
2021-07-18 14:18:12 -0500

    cambios

---
+++ pgapp/pgapp/doctype/sil_dpa/sil_dpa.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, orlando and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('sil_dpa', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pgapp/pgapp/doctype/sil_dpa/sil_dpa.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, orlando and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class sil_dpa(Document):
+	def before_insert(self):
+		self.dpa_id=	self.dpa_anio + self.dpa_provincia + self.dpa_canton + self.dpa_parroquia
