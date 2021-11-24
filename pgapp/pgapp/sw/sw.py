140df2b98b7847c565dd389c0524fe06b5f34531
edwinorlando83 <edwinorlando83@gmail.com>
2021-11-24 23:30:50 +0000

    cambios

--- pgapp/pgapp/sw/sw.py
+++ pgapp/pgapp/sw/sw.py
@@ -125,6 +125,11 @@ def coneccionpg():
     db_version = cursor1.fetchall() #fetchone()
     cursor1.close()
     return db_version
+@frappe.whitelist( )
+def getVentaEmergente():
+    sql = """  select name,tipo,html,url from tabventana_emergente te where CURRENT_DATE BETWEEN te.fecha_inicio and te.fecha_fin   """
+    lst = frappe.db.sql(sql,as_dict=True)
+    return lst
 
 
  
---
+++ pgapp/silgeo/doctype/ventana_emergente/ventana_emergente.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, orlando and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ventana_emergente', {
+	// refresh: function(frm) {
+
+	// }
+});
