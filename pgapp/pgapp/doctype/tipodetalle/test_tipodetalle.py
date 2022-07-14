1048d674a6dc7a0b451925453a5dccc5bd6b35ee
ORLANDO <edwin_orlando83@gmail.com>
2022-07-14 14:29:59 -0500

    cambios en base

---
+++ pgapp/pgapp/doctype/tipodetalle/test_tipodetalle.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, orlando and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class Testtipodetalle(unittest.TestCase):
+	pass
---
+++ pgapp/pgapp/doctype/tipodetalle/tipodetalle.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, orlando and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('tipodetalle', {
+	// refresh: function(frm) {
+
+	// }
+});
