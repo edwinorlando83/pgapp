7f62384d0e4bd1e176ceb3d7b22824d7c921ab7a
ORLANDO <edwin_orlando83@gmail.com>
2022-07-19 13:43:17 -0500

    cambios

--- pgapp/pgapp/page/pag_serievalor/pag_serievalor.js
+++ pgapp/pgapp/page/pag_serievalor/pag_serievalor.js
@@ -23,72 +23,124 @@ frappe.pages['pag_serievalor'].on_page_load = function (wrapper) {
 			},
 			change: function () {
 				getIndicadores(periodo.get_value());
-			
+
 			}
 		}
 
 	});
 	periodo.refresh();
-	 
+
 }
 
-function dibujar(datos){
+function dibujar(datos) {
 	console.log(datos);
 
-	let html =`<table border=1 class="table1">
+	let html = `<table  class="ExcelTable2007">
 	<tbody>
 	  <tr>
-		<td>1</td>
-		<td ></td>
-		<td ></td>
-		<td ></td>
-		<td ></td>
-		<td ></td>
-		<td ></td>
-	  </tr>
- 
+	    <th class="heading">&nbsp;</th>
+		<th>Indicador</th>
+		<th >Provincia</th>
+		<th>Cantón</th>
+		<th>Parroquia</th>
+		<th>Meta</th>
+		<th>Valor</th>		 
+	  </tr> 
  `;
- 
- 
+ let i=1;
 	datos.forEach((row) => {
-		html +=` 
+		html += ` 
 		  <tr>
+		  <td > ${i}</td>
 			<td > ${row.ind_nombre}</td>
 			<td >${row.ind_nprovincia}</td>
 			<td >${row.ind_ncanton}</td>
 			<td >${row.ind_nparroquia}</td>
 		
-			<td > <input onchange="myFunction(this.value,'${row.name}' )"    value='${row.serval_meta}' > </input>   </td>
-			<td >  <input value='${row.serval_valor}' > </input>  </td>
-			<td > </td>
+			<td > <input  onkeypress="return isNumberKey(this, event);"  onchange=" actulizar_meta(this.value,'${row.name}' )"    value='${row.serval_meta}' > </input>   </td>
+			<td >  <input  onkeypress="return isNumberKey(this, event);"  onchange="actulizar_valor(this.value,'${row.name}' )"  value='${row.serval_valor}' > </input>  </td>
+			 
 		  </tr>
-		 `; 
-    });
+		 `;
+		 i++;
+	});
 
-	html +=` 
+	html += ` 
 	</tbody>
 	</table>`;
 
-	me.page.main.find(".div_datos").html( html);
+	me.page.main.find(".div_datos").html(html);
 
 
 }
 
-function getIndicadores(inperiod_codigo){
+function getIndicadores(inperiod_codigo) {
 
 	frappe.call({
-        "method": "pgapp.pgapp.page.pag_serievalor.pag_serievalor.getIndicadores",
-        args: {
-            period_codigo: inperiod_codigo,
-          
-        },
-        callback: function(r) {
+		"method": "pgapp.pgapp.page.pag_serievalor.pag_serievalor.getIndicadores",
+		args: {
+			period_codigo: inperiod_codigo,
+
+		},
+		callback: function (r) {
 
 			dibujar(r.message);
 
-        }
-    });
+		}
+	});
 }
-function myFunction(data ,name){
-alert(name)
+function actulizar_meta(inserval_meta, inname) {
+
+	frappe.call({
+		"method": "pgapp.pgapp.page.pag_serievalor.pag_serievalor.updateserie_meta",
+		args: {
+			name: inname,
+			serval_meta: inserval_meta,
+
+		},
+		callback: function (r) {
+			console.log(r.message);
+			frappe.show_alert('Se actualizo la información de meta', 1);
+
+		}
+	});
+
 }
+
+function actulizar_valor(inserval_valor, inname) {
+
+	frappe.call({
+		"method": "pgapp.pgapp.page.pag_serievalor.pag_serievalor.updateserie_valor",
+		args: {
+			name: inname,
+			serval_valor: inserval_valor,
+
+		},
+		callback: function (r) {
+			console.log(r.message);
+
+
+			frappe.show_alert('Se actualizo la información de Valor', 1);
+
+
+		}
+	});
+
+}
+
+function isNumberKey(txt, evt) {
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if (charCode == 46) {
+	  //Check if the text already contains the . character
+	  if (txt.value.indexOf('.') === -1) {
+		return true;
+	  } else {
+		return false;
+	  }
+	} else {
+	  if (charCode > 31 &&
+		(charCode < 48 || charCode > 57))
+		return false;
+	}
+	return true;
+  }
--- pgapp/pgapp/page/pag_serievalor/pag_serievalor.py
+++ pgapp/pgapp/page/pag_serievalor/pag_serievalor.py
@@ -4,6 +4,19 @@ import frappe
 def getIndicadores(period_codigo):
     sql =""" SELECT period_codigo , ind_nombre , ind_nprovincia, ind_ncanton , ind_nparroquia ,
          serval_meta , serval_valor , name
-        from tabsil_serievalor 
-        order by  period_codigo,ind_nombre,ind_nprovincia, ind_ncanton , ind_nparroquia  """.format(period_codigo)
+        from tabsil_serievalor   where period_codigo='{0}' 
+        order by  period_codigo,ind_nombre,ind_nprovincia, ind_ncanton , ind_nparroquia """.format(period_codigo)
     return frappe.db.sql(sql, as_dict=1)
+
+@frappe.whitelist()
+def updateserie_meta(name,serval_meta ):
+    obj = frappe.get_doc("sil_serievalor", name)
+    obj.serval_meta = serval_meta 
+    return obj.save()
+
+@frappe.whitelist()
+def updateserie_valor(name,serval_valor):
+    obj = frappe.get_doc("sil_serievalor", name)
+    obj.serval_valor = serval_valor 
+    return obj.save()
+
